8c49417ea61e5877cc5abfb7d5fbcdb95836ebd4
Crit1cal <89368503+Crit1cal@users.noreply.github.com>
2021-11-28 15:17:14 +0700

    first commit

---
+++ js/projectInfo.js
@@ -0,0 +1,36 @@
+const Projects = {
+    Details:{  
+        Template: {
+            ID: "Template",
+            Name: "",
+            SmallDescription: "",
+            LargeDescription: "",
+            DownloadLink: "",
+            EmbedVideoLinkID: "",
+            NoOfProjectImages: 1
+        },
+        TheLoopOfTheDeath: {
+            ID: "TheLoopOfTheDeath",
+            Name: "The Loop Of The Death",
+            SmallDescription: "Android, AI, Unity3D",
+            LargeDescription: 'The story of hopeless knight who need to get off the Loopless colosseum.<br>Play it for free. (Available in Android)',
+            DownloadLink: "https://drive.google.com/file/d/1W32X6FVvahuX7KgI02dXCiFkr5IVIZ6A/view?usp=sharing",
+            NoOfProjectImages: 3
+        },  
+        PixelatedGamesProject: {
+            ID: "PixelatedGamesProject",
+            Name: "Pixelated Games Project",
+            SmallDescription: "Shader, Tools, Unity3D",
+            LargeDescription: "Unity3D tool to make pixel game with any assets in Unity assets store.<br>This tool is incomplete.",
+            NoOfProjectImages: 2
+        },
+        DontStarveTogetherMods: {
+            ID: "DontStarveTogetherMods",
+            Name: "Don't Starve Together Mods",
+            SmallDescription: "Moding, Game,",
+            LargeDescription: 'Mods make the game more fun to play, or make this game to another game easily with the same concept.<br><br>• Pick Aix: combined between Pickaxe and Axe for easy use and you have more one slot.<br>• Baseballbat: hit evetything with baseballbat. It does a lot of damage.<br>• BAGUETTE!!: The France weapon I mean bread. It deals less damage to enemies.<br>• Wacky Wormhole: make wormhole any where you want. Makes you travel around map faster. <br>• Will have more in the future.   ',
+            DownloadLink: "https://drive.google.com/drive/folders/1-Uk8RyyXqn66vHAjsd_JBgTgCcTvrayO?usp=sharing",
+            NoOfProjectImages: 4
+        },
+    }
+};
